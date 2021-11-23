eac769fb328d3c14460ed5e147d34afd3eb78608
itsmarkodowd <16547497+itsmarkodowd@users.noreply.github.com>
2021-11-23 18:03:58 +0000

    Feat: leetcode x 1

---
+++ leetcode/1-two-sum.ts
@@ -0,0 +1,9 @@
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j && nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
