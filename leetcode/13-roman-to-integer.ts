8d070186c112bba102b135c830828d291b3af011
itsmarkodowd <16547497+itsmarkodowd@users.noreply.github.com>
2021-12-02 22:49:18 +0000

    Feat: 1 x easy leetcode

---
+++ leetcode/13-roman-to-integer.ts
@@ -0,0 +1,19 @@
+const romanValue = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+function romanToInt(s: string): number {
+  return s.split("").reduce((total, _, index) => {
+    if (romanValue[s[index]] < romanValue[s[index + 1]]) {
+      return total - romanValue[s[index]];
+    } else {
+      return total + romanValue[s[index]];
+    }
+  }, 0);
+}
