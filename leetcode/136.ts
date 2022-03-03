7533c860e9ed183656b2b660e0882414ed5f5443
itsmarkodowd <16547497+itsmarkodowd@users.noreply.github.com>
2022-03-03 21:57:47 +0000

    Feat: 1 x easy

---
+++ leetcode/136.ts
@@ -0,0 +1,10 @@
+function singleNumber(nums: number[]): number {
+  //   create map counting number of times each item appeared
+  let countMap = nums.reduce((el, idx) => {
+    el[idx] = el[idx] ? el[idx] + 1 : 1
+    return el
+  }, {})
+
+  // find the key with a value of 1 appearance
+  return Number(Object.keys(countMap).find((key) => countMap[key] === 1))
+}
