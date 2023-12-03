71997054ba1edd5365ecec01cde9d673be8c55d5
Mark O'Dowd <contact@markodowd.dev>
2023-12-03 23:02:51 +0000

    feat: final 2 beginner leetcode tutorial

---
+++ leetcode/typescript/383.ts
@@ -0,0 +1,25 @@
+function canConstruct(ransomNote: string, magazine: string): boolean {
+  const magazineMap = new Map<string, number>()
+
+  // Create a map counting the number of each character in the magazine
+  for (let char of magazine) {
+    magazineMap.set(char, (magazineMap.get(char) ?? 0) + 1)
+  }
+
+  // Check if the ransom note can be constructed using the number of characters in the magazine
+  for (let char of ransomNote) {
+    if (!magazineMap.has(char)) {
+      return false
+    }
+
+    const charCount = magazineMap.get(char) ?? 0
+
+    if (!charCount) {
+      return false
+    }
+
+    magazineMap.set(char, charCount - 1)
+  }
+
+  return true
+}
---
+++ leetcode/typescript/876.ts
@@ -0,0 +1,38 @@
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
+}
+
+function middleNode(head: ListNode | null): ListNode | null {
+  // If there is no head return null
+  if (!head) {
+    return null
+  }
+
+  // If there is only one node can just return it
+  if (!head.next) {
+    return head
+  }
+
+  // Two pointers
+  let middleNode = head
+  let endNode = head
+
+  // Iterate through the list until the endNode reaches the end (endNode === null)
+  while (endNode && endNode.next) {
+    // Move the middleNode one step forward
+    if (middleNode.next) {
+      middleNode = middleNode.next
+    }
+    // Move the endNode two steps forward
+    if (endNode.next.next) {
+      endNode = endNode.next.next
+    }
+  }
+
+  return middleNode
+}
