5d11f8ef2ab274e3a9e70f4183183786272c5220
Mark O'Dowd <contact@markodowd.dev>
2025-07-18 23:58:36 +0100

    feat: leetcode

---
+++ leetcode/python/26.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        writer = 1
+
+        for reader in range(1, len(nums)):
+            if nums[reader] != nums[reader - 1]:
+                nums[writer] = nums[reader]
+                writer += 1
+
+        return writer
