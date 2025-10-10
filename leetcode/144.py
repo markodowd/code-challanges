03500c12fea93e15a38eeb411111b9146ede96e3
Mark O'Dowd <contact@markodowd.dev>
2025-10-10 14:25:44 +0100

    feat: leet

---
+++ leetcode/144.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+
+        def preorder(root):
+            if not root:
+                return
+            
+            result.append(root.val)
+            preorder(root.left)
+            preorder(root.right)
+        
+        preorder(root)
+
+        return result
+
