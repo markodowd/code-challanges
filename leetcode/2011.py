a7a5b137dd510ef5f2979b1fc4467359fe131c04
Mark O'Dowd <contact@markodowd.dev>
2025-10-21 23:11:44 +0100

    feat: leet

---
+++ leetcode/2011.py
@@ -0,0 +1,16 @@
+operations_dict = {
+    "++X": 1,
+    "X++": 1,
+    "--X": -1,
+    "X--": -1
+}
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        result = 0
+
+        for operation in operations:
+            result += operations_dict[operation]
+
+        return result
+
