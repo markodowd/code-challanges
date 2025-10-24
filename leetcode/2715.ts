a83034adf87e2ec826036dd0e9d6ce274a7a07d5
Mark O'Dowd <contact@markodowd.dev>
2025-10-24 16:32:18 +0100

    feat: leet

---
+++ leetcode/2715.ts
@@ -0,0 +1,11 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (...args: JSONValue[]) => void
+
+function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
+    const delayed_fn = setTimeout(() => fn(...args), t)
+
+    const cancelFn = () => clearTimeout(delayed_fn)
+
+    return cancelFn
+}
+
