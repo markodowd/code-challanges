1506ff44743e540196fb0d5071613279e1c91619
Mark O'Dowd <contact@markodowd.dev>
2025-11-12 11:17:29 +0000

    feat: leet

---
+++ leetcode/1281.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        digits = str(n)
+
+        product = int(digits[0])
+        sum_digits = int(digits[0])
+
+        for i in range(1, len(digits)):
+            product *= int(digits[i])
+            sum_digits += int(digits[i])
+
+        return product - sum_digits
