360f081bb36bc41e345f1e57e3859eb81d62de5a
Mark O'Dowd <contact@markodowd.dev>
2025-11-13 15:42:09 +0000

    feat: leet

---
+++ leetcode/1662.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        join_1 = "".join(word1)
+        join_2 = "".join(word2)
+
+        return join_1 == join_2
