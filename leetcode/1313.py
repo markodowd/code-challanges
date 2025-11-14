968fcb7534589bb076ffb4098ba04886e399d7e8
Mark O'Dowd <contact@markodowd.dev>
2025-11-14 15:57:13 +0000

    feat: leet

---
+++ leetcode/1313.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        answer = []
+
+        for i in range(0, len(nums), 2):
+            arr = [nums[i+1]] * nums[i]
+            answer.extend(arr)
+
+        return answer
