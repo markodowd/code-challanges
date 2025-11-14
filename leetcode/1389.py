9fbd200e84ac2938ac7f927ab6bb14a69557f1d7
Mark O'Dowd <contact@markodowd.dev>
2025-11-14 17:42:31 +0000

    feat: leet

---
+++ leetcode/1389.py
@@ -0,0 +1,9 @@
+class Solution:
+    def createTargetArray(self, nums: list[int], index: list[int]) -> list[int]:
+        target = []
+
+        for i in range(len(nums)):
+            target.insert(index[i], nums[i])
+
+        return target
+        
