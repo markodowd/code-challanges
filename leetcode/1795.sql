b321d53a1912faf64f18c6dbe988e178c3d10009
Mark O'Dowd <contact@markodowd.dev>
2025-11-14 17:58:41 +0000

    feat: leet

---
+++ leetcode/1795.sql
@@ -0,0 +1,16 @@
+/*
+Author: Mark O'Dowd
+Email: contact@markodowd.dev
+LeetCode: https://leetcode.com/u/markodowd
+ */
+SELECT
+    product_id,
+    s.store,
+    s.price
+FROM
+    products p
+    CROSS JOIN LATERAL unnest(ARRAY['store1', 'store2', 'store3'], ARRAY[p.store1, p.store2, p.store3]) AS s (store,
+        price)
+WHERE
+    s.price IS NOT NULL;
+
