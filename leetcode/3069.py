94d0ea37685c44b48b10d2733221857e6a286e9c
Mark O'Dowd <contact@markodowd.dev>
2026-08-25 14:40:57 +0100

    feat: leet tests

--- leetcode/3069.py
+++ leetcode/3069.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 class Solution:
     def resultArray(self, nums: list[int]) -> list[int]:
         ans_1 = [nums[0]]
@@ -10,3 +13,18 @@ class Solution:
                 ans_2.append(nums[i])
 
         return ans_1 + ans_2
+
+
+class TestResultArray(unittest.TestCase):
+    def setUp(self) -> None:
+        self.solver = Solution()
+
+    def test_1(self):
+        self.assertEqual(self.solver.resultArray([2, 1, 3]), [2, 3, 1])
+
+    def test_2(self):
+        self.assertEqual(self.solver.resultArray([5, 4, 3, 8]), [5, 3, 4, 8])
+
+
+if __name__ == "__main__":
+    unittest.main()
