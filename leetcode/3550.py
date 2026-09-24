0fe9b63ad692765a17360dc78b349565a18fcecd
Mark O'Dowd <contact@markodowd.dev>
2026-09-24 13:34:03 +0100

    feat: leet

--- leetcode/3550.py
+++ leetcode/3550.py
@@ -1,12 +1,34 @@
+import unittest
+
+
 class Solution:
     def smallestIndex(self, nums: list[int]) -> int:
-        output = -1
+        for i, num in enumerate(nums):
+            digit_sum = 0
 
-        for x in range(len(nums)):
-            digit_sum = sum(int(x) for x in str(nums[x]))
+            while num > 0:
+                digit_sum += num % 10
+                num //= 10
 
-            if x == digit_sum:
-                output = x
-                break
+            if i == digit_sum:
+                return i
 
-        return output
+        return -1
+
+
+class TestSmallestIndex(unittest.TestCase):
+    def setUp(self) -> None:
+        self.solver = Solution()
+
+    def test_1(self):
+        self.assertEqual(self.solver.smallestIndex([1, 3, 2]), 2)
+
+    def test_2(self):
+        self.assertEqual(self.solver.smallestIndex([1, 10, 11]), 1)
+
+    def test_3(self):
+        self.assertEqual(self.solver.smallestIndex([1, 2, 3]), -1)
+
+
+if __name__ == "__main__":
+    unittest.main()
